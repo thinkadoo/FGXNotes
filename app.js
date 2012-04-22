f42b6848dd31fd4f4f7604da894ee64894286608
Andre Venter <andre.n.venter@gmail.com>
2012-04-22 12:36:08 +0200

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+Ext.application({
+    name: "NotesApp",
+
+    models: ["Note"],
+    stores: ["Notes"],
+    controllers: ["Notes"],
+    views: ["NotesList", "NotesListContainer", "NoteEditor"],
+
+    launch: function () {
+
+        var notesListContainer = {
+            xtype: "noteslistcontainer"
+        };
+        var noteEditor = {
+            xtype: "noteeditor"
+        };
+
+        Ext.Viewport.add([notesListContainer, noteEditor]);
+
+    }
+});
